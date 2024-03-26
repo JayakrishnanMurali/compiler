fb2d6f97438ca3b3afbf3f8aec2cabce5705713e
Jayakrishnan M <jayakrishnan1012@gmail.com>
2024-03-26 16:11:07 +0530

    Add ReturnStatement to AST and Parser

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -58,6 +58,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -100,4 +102,17 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
 	return p.curToken.Type == t
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// We're skipping the expression until we encounter a semicolon
+	for !p.curTokenIs((token.SEMICOLON)) {
+		p.nextToken()
+	}
+
+	return stmt
 }
